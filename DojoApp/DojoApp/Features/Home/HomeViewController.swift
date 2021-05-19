c773280235026149d6a9b9df7e8c84aeeb267e01
jessicalewinter <jessicalewinter@gmail.com>
2021-05-19 18:58:09 -0300

    Create initial configuration

---
+++ DojoApp/DojoApp/Features/Home/HomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  HomeViewController.swift
+//  DojoApp
+//
+//  Created by jessicalewinter on 19/05/21.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .blue
+    }
+}
